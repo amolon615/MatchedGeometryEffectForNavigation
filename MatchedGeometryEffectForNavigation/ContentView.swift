7ae31b5345ae1d5874ec11e84cc90a4c2c8ad741
Artem Horovenko <amolon210@icloud.com>
2025-03-04 14:33:51 +0000

    Initial Commit

---
+++ MatchedGeometryEffectForNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchedGeometryEffectForNavigation
+//
+//  Created by amolonus on 04/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchedGeometryEffectForNavigation/MatchedGeometryEffectForNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectForNavigationApp.swift
+//  MatchedGeometryEffectForNavigation
+//
+//  Created by amolonus on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectForNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
